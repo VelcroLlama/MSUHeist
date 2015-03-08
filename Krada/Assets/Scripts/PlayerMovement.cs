14dd6346f1f6e58f87776825ccb5850538107e46
deluksic <deluksic@windowslive.com>
2015-03-08 23:59:38 +0100

    Jumping implemented, physics gravity fixed.

--- Krada/Assets/Scripts/PlayerMovement.cs
+++ Krada/Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,9 @@ public class PlayerMovement : MonoBehaviour {
 	public float Speed;
 	public float SprintCoef;
 
+	private Vector3 DownSpeed;
+	private bool CanJump;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -16,12 +19,25 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if (CanJump && Input.GetAxis ("Jump") > 0) {
+			CanJump = false;
+			DownSpeed = Vector3.up;
+		}
 		this.transform.Rotate (0, Input.GetAxis ("Mouse X") * Time.deltaTime * Sensitivity, 0, Space.Self);
 		var speed = Vector3.zero;
 		var maxspeed = (Input.GetAxis ("Fire3") > 0 ? SprintCoef : 1) * Speed;
 		speed += Vector3.forward * Input.GetAxis ("Vertical") * maxspeed;
 		speed += Vector3.right * Input.GetAxis ("Horizontal") * maxspeed;
 		speed = Vector3.ClampMagnitude (speed, maxspeed);
-		controller.SimpleMove (this.transform.rotation * speed);
+		DownSpeed += Physics.gravity * Time.deltaTime;
+		speed += DownSpeed;
+		controller.Move (this.transform.rotation * speed * Time.deltaTime);
+	}
+
+	void OnControllerColliderHit(ControllerColliderHit hit) {
+		if (hit.collider.name == "Floor") {
+			DownSpeed = Vector3.zero;
+			CanJump = true;
+		}
 	}
 }
