ee1e051c5a557cac5a9063ad99f6bba5084fe9de
Juraj Pejnović <furgonprime@gmail.com>
2015-03-08 22:54:52 +0100

    scene 2 player placed

--- Krada/Assets/Scripts/PauseControl.cs
+++ Krada/Assets/Scripts/PauseControl.cs
@@ -3,9 +3,24 @@ using System.Collections;
 
 public class PauseControl : MonoBehaviour {
 
+    public Animator pause;
     void Update () {
-	
-	}
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (pause.GetBool("Open"))
+            {
+                pause.SetBool("Open", false);
+                Time.timeScale = 1;
+            }
+            else
+            {
+                pause.SetBool("Open", true);
+                Time.timeScale = 0;
+            }
+        }
+        
+    
+    }
 
     public void Exit()
     {
