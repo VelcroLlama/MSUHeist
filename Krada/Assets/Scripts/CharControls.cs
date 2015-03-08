bb29b837dce300d1779db57c066e584e3ff9aa49
Juraj Pejnović <furgonprime@gmail.com>
2015-03-08 14:40:28 +0100

    ui

--- Krada/Assets/Scripts/CharControls.cs
+++ Krada/Assets/Scripts/CharControls.cs
@@ -3,10 +3,9 @@ using System.Collections;
 
 public class CharControls : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    public CharacterController Player1;
+    public CharacterController Player2;
+    public CharacterController Player3;
 	
 	// Update is called once per frame
 	void Update () {
