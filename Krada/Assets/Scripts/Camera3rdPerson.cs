e48729b01fe828cb3b30af5e54745ff01873a04f
David Emanuel Luksic <deluksic@gmail.com>
2015-03-08 19:49:56 +0100

    Cool new player prefab.

---
+++ Krada/Assets/Scripts/Camera3rdPerson.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera3rdPerson : MonoBehaviour {
+
+	private Vector3 PositionDelta;
+	private Transform Target;
+
+	// Use this for initialization
+	void Start () {
+		PositionDelta = transform.localPosition;
+		Target = transform.parent;
+		transform.parent = null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.rotation = Quaternion.Euler (
+			transform.rotation.eulerAngles.x, 
+		    Mathf.LerpAngle (transform.rotation.eulerAngles.y, Target.rotation.eulerAngles.y, 0.8f), 
+		    transform.rotation.eulerAngles.z
+		);
+		transform.position = Vector3.Lerp (transform.position, Target.position+Target.rotation*PositionDelta, 0.2f);
+	}
+}
---
+++ Krada/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	
+	public CharacterController controller;
+
+	public float Sensitivity;
+	public float Speed;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Rotate (0, Input.GetAxisRaw ("Mouse X") * Sensitivity, 0, Space.Self);
+		var speed = Vector3.zero;
+		speed += Vector3.forward * Input.GetAxis ("Vertical") * Speed;
+		speed += Vector3.right * Input.GetAxis ("Horizontal") * Speed;
+		speed = Vector3.ClampMagnitude (speed, Speed);
+		controller.SimpleMove (this.transform.rotation * speed);
+	}
+}
