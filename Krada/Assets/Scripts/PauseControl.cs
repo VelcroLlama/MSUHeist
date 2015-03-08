6fd51f56fa75a6a4cebc5af6d70dec01ea01447b
Juraj Pejnović <furgonprime@gmail.com>
2015-03-08 22:11:19 +0100

    Animations

---
+++ Krada/Assets/Scripts/PauseControl.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseControl : MonoBehaviour {
+
+    void Update () {
+	
+	}
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
