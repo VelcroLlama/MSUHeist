e1585a28d41ef8ed65a1e45d3d40071c02348139
deluksic <deluksic@windowslive.com>
2015-03-08 20:31:28 +0100

    Fixed mouse input

--- Krada/Assets/Scripts/PlayerMovement.cs
+++ Krada/Assets/Scripts/PlayerMovement.cs
@@ -16,7 +16,7 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		this.transform.Rotate (0, Input.GetAxisRaw ("Mouse X") * Sensitivity, 0, Space.Self);
+		this.transform.Rotate (0, Input.GetAxis ("Mouse X") * Sensitivity, 0, Space.Self);
 		var speed = Vector3.zero;
 		speed += Vector3.forward * Input.GetAxis ("Vertical") * Speed;
 		speed += Vector3.right * Input.GetAxis ("Horizontal") * Speed;
