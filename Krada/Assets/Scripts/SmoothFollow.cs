254e8aac4796c9ff23ebcc2f87f384b4d0dc8968
deluksic <deluksic@windowslive.com>
2015-03-09 19:53:58 +0100

    Camera climbs up walls

--- Krada/Assets/Scripts/SmoothFollow.cs
+++ Krada/Assets/Scripts/SmoothFollow.cs
@@ -21,27 +21,34 @@ public class SmoothFollow : MonoBehaviour {
 	// Update is called once per frame
 	void LateUpdate () {
 		
-		float wantedRotationAngle = Target.eulerAngles.y;
-		float currentRotationAngle = transform.eulerAngles.y;
+		float rotationAngle = Target.eulerAngles.y;
 		// Damp the rotation around the y-axis
-		currentRotationAngle = Mathf.LerpAngle (currentRotationAngle, wantedRotationAngle, Time.deltaTime / Smoothness);
-		var currentRotation = Quaternion.Euler(0, currentRotationAngle, 0);
+		var currentRotation = Quaternion.Euler(0, rotationAngle, 0);
 
 		// distance meters behind the target
-		transform.position = Target.position;
-		transform.position += currentRotation * PositionDelta;
-
-		transform.LookAt (Target.position);
+		var wantedPosition = Target.position;
+		wantedPosition += currentRotation * PositionDelta;
 
 		RaycastHit hit;
-		var direction = transform.position - Target.position;
+		var direction = wantedPosition - Target.position;
+		var offset = 0.05f;
 		direction.Normalize ();
-		Physics.Raycast (Target.position + direction * 0.1f, direction * 0.9f, out hit, PositionDelta.magnitude);
-		if (hit.collider)
-			this.transform.position = hit.point - direction * 0.1f;
-		
+		Physics.Raycast (Target.position + direction * offset, direction, out hit, PositionDelta.magnitude);
+		if (hit.collider) {
+			var point = hit.point - direction * offset;
+			var distance = Vector3.Distance(Target.position, point);
+			var vx2 = new Vector3(direction.x, 0, direction.z).sqrMagnitude * distance;
+			point += 0.5f * Vector3.up * (Mathf.Sqrt(PositionDelta.sqrMagnitude - vx2));
+			wantedPosition = point;
+		}
+
+		transform.position = Vector3.Lerp (transform.position, wantedPosition, 0.1f);
+		transform.LookAt (Target.position);
+
 		verticalRotation -= Input.GetAxis ("Mouse Y") * Time.deltaTime * VerticalSensitivity;
-		verticalRotation = Mathf.Clamp (verticalRotation, -20, 20);
-		transform.rotation = Quaternion.Euler (verticalRotation, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+		verticalRotation = Mathf.Clamp (verticalRotation, -20, +40);
+		transform.rotation = Quaternion.Euler (verticalRotation, 
+		                                       transform.rotation.eulerAngles.y, 
+		                                       transform.rotation.eulerAngles.z);
 	}
 }
